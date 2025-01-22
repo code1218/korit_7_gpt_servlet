c10876d3a5be5ca4c8b07c4cd03a0d9273c96c80
code1218 <skjil1218@gmail.com>
2025-01-22 14:50:18 +0900

    회원가입 암호화 전

--- src/main/java/com/korit/servlet_study/dto/SignupDto.java
+++ src/main/java/com/korit/servlet_study/dto/SignupDto.java
@@ -1,5 +1,34 @@
 package com.korit.servlet_study.dto;
 
-public class SignupDto {
+import com.korit.servlet_study.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupDto {
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+
+    public User toUser() {
+        return User.builder()
+                .username(username)
+                .password(password)
+                .name(name)
+                .email(email)
+                .build();
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/com/korit/servlet_study/service/AuthService.java
+++ src/main/java/com/korit/servlet_study/service/AuthService.java
@@ -1,12 +1,31 @@
 package com.korit.servlet_study.service;
 
+import com.korit.servlet_study.dao.AuthDao;
 import com.korit.servlet_study.dto.ResponseDto;
 import com.korit.servlet_study.dto.SignupDto;
+import com.korit.servlet_study.entity.User;
 
 public class AuthService {
+    private AuthDao authDao;
+
+    private static AuthService instance;
+
+    private AuthService() {
+        authDao = AuthDao.getInstance();
+    }
+
+    public static AuthService getInstance() {
+        if (instance == null) {
+            instance = new AuthService();
+        }
+        return instance;
+    }
 
     public ResponseDto<?> signup(SignupDto signupDto) {
-
-        return ResponseDto.success();
+        User insertedUser = authDao.signup(signupDto.toUser());
+        if(insertedUser == null) {
+            return ResponseDto.fail("사용자를 추가하지 못하였습니다.");
+        }
+        return ResponseDto.success(insertedUser);
     }
 }
